b80da8a2e7c03c05091a3192c514ec554af0bf43
Colleen Skaroulis <cskaroulis@yahoo.com>
2022-10-05 14:27:30 -0400

    added a new index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+
+// serve static files from public directory
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+app.listen(3000, function () {
+  console.log("Running on port: 3000");
+});
